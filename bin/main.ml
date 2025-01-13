5cc8cfcfaa6ecfec0fc6a810c2ce8fcc7604efe5
Daria Vasina <dash.vasina@gmail.com>
2025-01-14 02:44:37 +0300

    small fixes

--- bin/main.ml
+++ bin/main.ml
@@ -1,56 +1,64 @@
 open Interpolation
 open Io
 
-let print_interpolation_result points interpolation_types step =
-  List.iter (fun interpolation_type ->
+type runner = {
+  step : float;
+  points_stream : (float * float) Seq.t;
+  interpolation_types : interpolation list;
+}
+
+let generate_x_values x_min x_max step =
+  let rec aux current =
+    if current > x_max then Seq.Nil
+    else Seq.Cons (current, fun () -> aux (current +. step))
+  in
+  fun () -> aux x_min
+
+let perform_interpolation points interpolation_types step =
+  List.iter (fun interpolation ->
     let result =
-      match interpolation_type with
-      | `Linear -> linear_interpolation points step
-      | `Lagrange -> lagrange_interpolation points step
+      generate_x_values (fst (List.hd points)) (fst (List.hd (List.rev points))) step
+      |> Seq.map (fun x -> (x, interpolation.interpolate points x))
+      |> List.of_seq
     in
-    let result_list = List.of_seq result in
-    let x_values = List.map (fun (x, _) -> Printf.sprintf "%.2f" x) result_list in
-    let y_values = List.map (fun (_, y) -> Printf.sprintf "%.2f" y) result_list in
-    let header = 
-      match interpolation_type with
-      | `Linear -> "Линейная"
-      | `Lagrange -> "Лагранж"
-    in
-    print_endline header;
+    let x_values = List.map (fun (x, _) -> Printf.sprintf "%.2f" x) result in
+    let y_values = List.map (fun (_, y) -> Printf.sprintf "%.2f" y) result in
+    print_endline interpolation.name;
     print_endline (String.concat "\t" x_values);
     print_endline (String.concat "\t" y_values);
   ) interpolation_types
 
-let rec run_interpolation points interpolation_types step =
-  print_interpolation_result points interpolation_types step;
+let rec update_runner runner =
+  let points = List.of_seq runner.points_stream in
+  perform_interpolation points runner.interpolation_types runner.step;
   let new_point = read_point () in
-  let updated_points = points @ [new_point] in
-  let required_points = if List.mem `Linear interpolation_types then 2 else 3 in
+  let updated_points = Seq.append runner.points_stream (Seq.return new_point) in
+  let required_points = List.fold_left (fun acc interpolation -> max acc interpolation.window_size) 0 runner.interpolation_types in
   let updated_points =
-    if List.length updated_points > required_points then List.tl updated_points else updated_points
+    if Seq.length updated_points > required_points then Seq.drop 1 updated_points else updated_points
   in
-  run_interpolation updated_points interpolation_types step
+  update_runner { runner with points_stream = updated_points }
 
-let parse_args args current_step interpolation_types =
+let parse_arguments args =
   let rec aux args current_step interpolation_types =
     match args with
-    | [] -> interpolation_types, current_step
-    | "-m" :: "linear" :: rest -> aux rest current_step (`Linear :: interpolation_types)
-    | "-m" :: "lagrange" :: rest -> aux rest current_step (`Lagrange :: interpolation_types)
+    | [] -> { step = current_step; points_stream = Seq.empty; interpolation_types }
+    | "-m" :: "linear" :: rest -> aux rest current_step (linear_interpolation :: interpolation_types)
+    | "-m" :: "lagrange" :: rest -> aux rest current_step (lagrange_interpolation :: interpolation_types)
     | "-s" :: step_value :: rest -> aux rest (float_of_string step_value) interpolation_types
     | _ :: rest -> aux rest current_step interpolation_types
   in
-  aux args current_step interpolation_types
+  aux args 1.0 []
 
-let () =
-  let interpolation_types, step =
-    let args = Array.to_list Sys.argv |> List.tl in
-    parse_args args 1.0 []
-  in
-  let required_points = if List.mem `Linear interpolation_types then 2 else 3 in
+let initialize_runner () =
+  let args = Array.to_list Sys.argv |> List.tl in
+  let runner = parse_arguments args in
+  let required_points = List.fold_left (fun acc interpolation -> max acc interpolation.window_size) 0 runner.interpolation_types in
   let rec read_initial_points n acc =
-    if n = 0 then List.rev acc
-    else read_initial_points (n - 1) (read_point () :: acc)
+    if n = 0 then acc
+    else read_initial_points (n - 1) (Seq.append acc (Seq.return (read_point ())))
   in
-  let initial_points = read_initial_points required_points [] in
-  run_interpolation initial_points interpolation_types step
+  let initial_points = read_initial_points required_points Seq.empty in
+  update_runner { runner with points_stream = initial_points }
+
+let () = initialize_runner ()
--- lib/interpolation/interpolation.ml
+++ lib/interpolation/interpolation.ml
@@ -1,42 +1,34 @@
-let generate_points x_min x_max step =
-  let rec aux current =
-    if current > x_max then Seq.Nil
-    else Seq.Cons (current, fun () -> aux (current +. step))
-  in
-  fun () -> aux x_min
+type interpolation = {
+  name : string;
+  window_size : int;
+  interpolate : (float * float) list -> float -> float;
+}
 
-let linear_interpolation points step =
-  let interpolate_y x points =
-    let rec find_interval = function
-      | (x1, y1) :: (x2, y2) :: _ when x1 <= x && x <= x2 -> Some (x1, y1, x2, y2)
-      | _ :: rest -> find_interval rest
-      | [] -> None
-    in
-    match find_interval points with
-    | Some (x1, y1, x2, y2) ->
-        y1 +. (y2 -. y1) *. (x -. x1) /. (x2 -. x1)
-    | None -> failwith "Не удалось интерполировать точку"
-  in
-  let x_min, _ = List.hd points in
-  let x_max, _ = List.hd (List.rev points) in
-  generate_points x_min x_max step
-  |> Seq.map (fun x -> (x, interpolate_y x points))
+let linear_interpolate points x =
+  match points with
+  | [ (x0, y0); (x1, y1) ] ->
+      ((x -. x0) *. (y1 -. y0) /. (x1 -. x0)) +. y0
+  | _ -> failwith "Invalid set of points"
 
-let lagrange_interpolation points step =
-  let lagrange_polynomial x points =
-    List.fold_left
-      (fun acc (xi, yi) ->
-        let li =
-          List.fold_left
-            (fun prod (xj, _) ->
-              if Float.equal xi xj then prod
-              else prod *. (x -. xj) /. (xi -. xj))
-            1.0 points
-        in
-        acc +. yi *. li)
-      0.0 points
-  in
-  let x_min, _ = List.hd points in
-  let x_max, _ = List.hd (List.rev points) in
-  generate_points x_min x_max step
-  |> Seq.map (fun x -> (x, lagrange_polynomial x points))
+let lagrange_interpolate points x =
+  List.fold_left
+    (fun acc (xi, yi) ->
+      acc
+      +. yi
+         *. List.fold_left
+              (fun prod (xj, _) ->
+                if xj = xi then prod else prod *. (x -. xj) /. (xi -. xj))
+              1. points)
+    0. points
+
+let linear_interpolation = {
+  name = "Linear";
+  window_size = 2;
+  interpolate = linear_interpolate;
+}
+
+let lagrange_interpolation = {
+  name = "Lagrange";
+  window_size = 3;
+  interpolate = lagrange_interpolate;
+}
