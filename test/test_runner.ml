f563b87f31db2c02a3279db2a00a1e2272e07d53
Daria Vasina <dash.vasina@gmail.com>
2025-01-17 20:06:11 +0300

    fixed test

--- test/test_runner.ml
+++ test/test_runner.ml
@@ -6,5 +6,5 @@ let test_generate_x_values () =
   let x_max = 2.5 in
   let step = 1. in
   let result = generate_x_values x_min x_max step |> List.of_seq in
-  check (list (float 0.01)) "generate x values" [ 0.; 1.; 2.; 3. ] result
+  check (list (float 0.01)) "generate x values" [ 0.; 1.; 2. ] result
 
